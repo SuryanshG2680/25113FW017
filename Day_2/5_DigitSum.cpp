b1c6d8cf2026e7d9750f8e596f7d45f66374d1a0
Suryansh Gupta <suryanshg2680@gmail.com>
2026-05-30 16:44:55 +0530

    Implement digit sum calculation in C++

--- Day_2/5_DigitSum.cpp
+++ Day_2/5_DigitSum.cpp
@@ -1,1 +1,19 @@
 //Sum of digits of a number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, c, sum=0;
+    cout << "Enter N: ";
+    cin >> n;
+
+    for(int x=n; x>0; x/=10){
+      c= x%10;  
+      sum=sum+c;
+    }
+    cout << "Sum of digits = " << sum;
+
+    return 0;
+}
